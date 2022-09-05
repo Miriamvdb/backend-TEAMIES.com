22b50d560f5293ec2b266659a762aa6b50a5d85b
Miriamvdb <miriamvdb@live.nl>
2022-09-05 14:25:21 +0200

    Back: First setup

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const { PORT } = require("./config/constants");
 // Create an express app
 const app = express();
 
-
 // CORS middleware:  * Since our api is hosted on a different domain than our client
 // we are are doing "Cross Origin Resource Sharing" (cors)
 // Cross origin resource sharing is disabled by express by default
@@ -20,11 +19,10 @@ app.use(corsMiddleWare());
 // express.json() to be able to read request bodies of JSON requests a.k.a. body-parser
 app.use(express.json());
 
-
 //routes
 app.use("/auth", authRouter);
 
 //start listening
 app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+  console.log(`TEAMIES.com - Listening on port: ${PORT}`);
 });
