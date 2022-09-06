462b7b179b6cb61506662dbc49406f2a0472a165
Miriamvdb <miriamvdb@live.nl>
2022-09-06 21:39:42 +0200

    Back/F2: GET all events incl. category

--- routers/event.js
+++ routers/event.js
@@ -3,4 +3,17 @@ const Category = require("../models").category;
 const Event = require("../models").event;
 const router = new Router();
 
+// F2 - GET all events incl. corresponding category - http GET :4000/events
+router.get("/", async (req, res, next) => {
+  try {
+    const allEvents = await Event.findAll({
+      include: [Category],
+    });
+    res.send(allEvents);
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
 module.exports = router;
