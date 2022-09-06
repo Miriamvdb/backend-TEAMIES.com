5e14267997bc4b005f6193a737a8f88b505c6a59
Miriamvdb <miriamvdb@live.nl>
2022-09-06 14:41:21 +0200

    Front/F1: GET all players incl. team - no images yet

--- routers/player.js
+++ routers/player.js
@@ -1,6 +1,26 @@
 const { Router } = require("express");
 const Team = require("../models").team;
 const Player = require("../models").user;
+// const authMiddleware = require("../auth/middleware");
 const router = new Router();
+
+// F1 - GET all players incl. corresponding team - http GET :4000/players
+router.get("/", async (req, res, next) => {
+  try {
+    const allPlayers = await Player.findAll({
+      include: [
+        {
+          model: Team,
+          attributes: ["name"],
+        },
+      ],
+      attributes: { exclude: ["password"] },
+    });
+    res.send(allPlayers);
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
 
 module.exports = router;
