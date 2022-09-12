9f760714958fbc8a1c484fbf846fc53def2989ab
Miriamvdb <miriamvdb@live.nl>
2022-09-12 13:47:05 +0200

    Back/F7: Admin can create a new event

--- routers/event.js
+++ routers/event.js
@@ -63,4 +63,49 @@ router.patch(
   }
 );
 
+// F7: Admin can create a new event
+// http POST :4000/events/newevent/1 title=Training date="2022-10-24" startTime="19:15" endTime="20:45" Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2MjkxMDkwOSwiZXhwIjoxNjYyOTE4MTA5fQ.osVCdGfv817QaHFCJVIxjMeDhfnLEGp8tz0inxmpMro"
+router.post("/newevent/:categoryId", authMiddleware, async (req, res, next) => {
+  // Check if user is an admin
+  if (req.user.isAdmin !== true) {
+    return res
+      .status(403)
+      .send({ message: "You are not authorized to create a new event" });
+  }
+
+  const category = await Category.findByPk(req.params.categoryId);
+  console.log(category);
+
+  if (category === null) {
+    return res.status(404).send({ message: "This category does not exist" });
+  }
+
+  const { title, date, startTime, endTime, opponent, home, descr } = req.body;
+
+  if (!title || !date || !startTime || !endTime) {
+    return res
+      .status(400)
+      .send("Please provide a title, date, start time and an end time");
+  }
+
+  try {
+    const newEvent = await Event.create({
+      userId: req.user.id,
+      categoryId: category.id,
+      title,
+      date,
+      startTime,
+      endTime,
+      opponent,
+      home,
+      descr,
+    });
+
+    return res.status(201).send({ message: "New event created", newEvent });
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
 module.exports = router;
