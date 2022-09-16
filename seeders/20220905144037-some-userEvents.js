59dd16b1e17e7691289d531c7696ea7cd37c4942
Miriamvdb <miriamvdb@live.nl>
2022-09-16 10:53:41 +0200

    Comment out the userEvent seeders

--- seeders/20220905144037-some-userEvents.js
+++ seeders/20220905144037-some-userEvents.js
@@ -2,74 +2,73 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
-      "userEvents",
-      [
-        // user 1
-        {
-          participation: true,
-          userId: 1,
-          eventId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          participation: false,
-          userId: 1,
-          eventId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          participation: false,
-          userId: 1,
-          eventId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          participation: true,
-          userId: 1,
-          eventId: 4,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-
-        // user 2
-        {
-          participation: false,
-          userId: 2,
-          eventId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          participation: false,
-          userId: 2,
-          eventId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          participation: true,
-          userId: 2,
-          eventId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          participation: true,
-          userId: 2,
-          eventId: 4,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
+    // await queryInterface.bulkInsert(
+    //   "userEvents",
+    //   [
+    //     // user 1
+    //     {
+    //       participation: true,
+    //       userId: 1,
+    //       eventId: 1,
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       participation: false,
+    //       userId: 1,
+    //       eventId: 2,
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       participation: false,
+    //       userId: 1,
+    //       eventId: 3,
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       participation: true,
+    //       userId: 1,
+    //       eventId: 4,
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     // user 2
+    //     {
+    //       participation: false,
+    //       userId: 2,
+    //       eventId: 1,
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       participation: false,
+    //       userId: 2,
+    //       eventId: 2,
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       participation: true,
+    //       userId: 2,
+    //       eventId: 3,
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       participation: true,
+    //       userId: 2,
+    //       eventId: 4,
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //   ],
+    //   {}
+    // );
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("userEvents", null, {});
+    // await queryInterface.bulkDelete("userEvents", null, {});
   },
 };
